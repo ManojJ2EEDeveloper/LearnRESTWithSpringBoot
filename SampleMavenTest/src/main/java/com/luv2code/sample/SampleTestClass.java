67f02319ad7e2663d3bcb17fbd3f0cc791c57b15
Manoj <Manoj@LAPTOP-GQNSINI6>
2019-01-01 17:30:36 +0530

    First Commit

---
+++ SampleMavenTest/src/main/java/com/luv2code/sample/SampleTestClass.java
@@ -0,0 +1,8 @@
+package com.luv2code.sample;
+
+public class SampleTestClass {
+	public static void main(String[] args) {
+		System.out.println("Hello Git!!!");
+		System.out.println("Pushing Test Project t Git!!");
+	}
+}
